514d40f8cbf39c063a98866023a93403256854c3
LIWEI WANG <lwang74@bluehive.bluehive.circ.private>
2020-04-22 10:54:41 -0400

    change for bluehive also

--- apds/apds.m
+++ apds/apds.m
@@ -5,32 +5,32 @@
 %1:6252, third column is intensity.
 clearvars
 codefolder=pwd;
-%solvent='F8T2O2';
-%srdir=['/scratch/lwang74/PTU_spectrum_lifetime_bluehive/PTUdata/' solvent];
-srdir=['E:\F8T2N2'];
+solvent='MEH_CH';
+srdir=['/scratch/lwang74/PTU_spectrum_lifetime_bluehive/PTUdata/' solvent];
+%srdir=['E:\F8T2N2'];
 cd (srdir)
 
 
-allnames=struct2cell(dir( '*02042019*4d1d1.mat'));
+allnames=struct2cell(dir( '*2019*.mat'));
 [~,len]=size(allnames);
 for len_i=1:1:len
-    clearvars -except srdir codefolder solvent len_i len allnames
+    clearvars -except srdir codefolder solvent len_i len allnames solvent
     datasetname=char(allnames(1,len_i));
-    datasetfile=load([srdir '\' datasetname]);
+    datasetfile=load([srdir '/' datasetname]);
     rowrange=datasetfile.dataset.rowrange;clearvars datasetfile
     disp('Finish load rowrange /n')    
     
     date=regexp(datasetname,'\d*2019','match');
     file=regexp(datasetname,'\dd\dd\d*','match');
     
-    cd([srdir '\apd full'])
+    cd([srdir '/apd full'])
     apdfile=dir(['*' date{1} '*' file{1} '.mat']);
     if isempty(apdfile)
         disp(['Wrong apd related to' datasetfile])
     else
         cd(codefolder)
         clearvars apddata apddataresolution
-        [apddata,apddataresolution]=PTUim([apdfile.folder '\' apdfile.name]);
+        [apddata,apddataresolution]=PTUim([apdfile.folder '/' apdfile.name]);
     end
     datasource=GetDandABS(apddata,0,'M');
     absolutetime=datasource(:,1);
@@ -58,7 +58,7 @@ for len_i=1:1:len
        end
     end
     
-cd([srdir '\apd full'])
-save(['F8T2 Chloroform 2kDa N2 ' date{1} ' SecDtime ' file{1} '.mat'],'SecDtime');   
+cd([srdir '/apd full'])
+save([solvent ' ' date{1} ' SecDtime ' file{1} '.mat'],'SecDtime');   
     
 end
